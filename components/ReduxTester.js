5cf1429aabb55fab58e7b9b32043530eaec07432
Sean Hsieh <sean.hsieh@garmin.com>
2021-01-27 14:19:38 +0800

    use React.memo
    Change-Id: I527c7bc2f025903e0cd65bf3fb0e2a6cc90db3b8

--- components/ReduxTester.js
+++ components/ReduxTester.js
@@ -1,51 +1,44 @@
-import React, { useEffect } from 'react';
-
-import {
-    useSelector,
-    useDispatch
-} from "react-redux";
-
-import {
-    getUsers,
-    getData
-} from "./actions/action";
-
-import {
-    selectUsers,
-    selectData
-} from "./states/states";
-
-import {
-    TemplateStyle,
-} from './styles/Style';
-
+import React from 'react';
 import useMonitor from 'use-react-monitor';
+
+const Results = ({ results , status, lastTimes}) => {
+    return (
+        <div>
+        {results && results.map((result, i) =>{
+            return (
+                <>
+                <div key={`lastTime-${i}`}> Last updated time: {lastTimes[i]}</div>
+                    <div key={`status-${i}`}>Status: {status && status[i]}</div>
+                    <ul key={i}>
+                        {result.data.map((r, index) => {
+                            return (<li key={index}>{r.id} {r.firstName} {r.lastName}</li>)
+                        })}
+                    </ul>
+                </>)
+        })}
+     </div>
+    );
+};
+
+const MemorizedResults = React.memo(Results);
 
 const ReduxTester = () => {
     const interval = 3000;
     const {results, status, lastTimes} = useMonitor(
         { urls:['http://rem-rest-api.herokuapp.com/api/users',
-                'http://rem-rest-api.herokuapp.com/api/users',
                 'http://rem-rest-api.herokuapp.com/api/users'],
           freshRate: interval});
 
     return (
-        <div>
-            {results.map((result, i) =>{
-                return (
-                    <>
-                        <div key={`lastTime-${i}`}> Last updated time: {lastTimes[i]}</div>
-                        <div key={`status-${i}`}>Status: {status[i]}</div>
-                        <ul key={i}>
-                            {result.data.map((r, index) => {
-                                return (<li key={index}>{r.id} {r.firstName} {r.lastName}</li>)
-                            })}
-                        </ul>
-                    </>)
-            })}
-         </div>
-
+        <>
+            <MemorizedResults results = {results}
+                              status = {status}
+                              lastTimes = {lastTimes}/>
+        </>
     )
 }
 
 export default ReduxTester
+
+
+
